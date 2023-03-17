ea6e0996b8062b6058e4287b48da765029696bdd
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-03-17 16:01:20 -0400

    added signIn feature

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors';
 import { json } from 'body-parser'
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -11,13 +12,19 @@ import { errorHandler } from './middlewares/error-handler'
 import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
+app.set('trust proxy', true) // trust traffic from ingress-nginx
 app.use(json())
+app.use(cookieSession({
+	signed: false, // disable encryption since JWT is already encrypted
+	secure: true, // only works on https connections
+}))
+
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
-// async code can throw errors, so we need to use next() to pass the error to the error handler
+// async code can throw errors, so we need to use next() function to pass the error to the error handler
 // but instead we can use the async express wrapper express-async-errors
 app.all('*', async (req, res) => {	
 	throw new NotFoundError()
@@ -26,6 +33,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler)
 
 const start = async () => {
+	if(!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be defined')
+	}
+
 	try {
 		// refers to cluster IP service in mongo-depl file
 		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
--- ticketing/auth/src/services/password.ts
+++ ticketing/auth/src/services/password.ts
@@ -1,12 +1,20 @@
 import { scrypt, randomBytes } from 'crypto';
 import { promisify } from 'util';
 
+const scryptAsync = promisify(scrypt);
+
 export class Password {
-	static toHash(password: string) {
-		
+	static async toHash(password: string) {
+		const salt = randomBytes(8).toString('hex');
+		const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+	
+		return `${buf.toString('hex')}.${salt}`;
 	}
 
-	static compare(storedPassword: string, suppliedPassword: string) {
-		return true;
+	static async compare(storedPassword: string, suppliedPassword: string) {
+		const [hashedPassword, salt] = storedPassword.split('.');
+		const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+		return buf.toString('hex') === hashedPassword;
 	}
 }
