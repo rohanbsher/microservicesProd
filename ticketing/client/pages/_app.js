dd2797306adcd792bc5b304fc1a639c0c5361c04
Rohan Bhandari <bhandarirohan556@gmail.com>
2023-04-03 15:11:06 -0400

    added info fetching ability
    added info fetching ability

--- ticketing/client/pages/_app.js
+++ ticketing/client/pages/_app.js
@@ -1,9 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import buildClient from '../api/build-client';
 
 // we have defined our own custom app component using _app file
 // A thin wrapper around the component that we are trying to show
 // Any global CSS that must be included will be included here since it is the only file
 // that will be started when we boot up
-export default ({ Component, pageProps }) => {
-	return <Component {...pageProps} />;
-}
+const AppsComponent = ({ Component, pageProps, currentUser }) => {
+	return (
+		<div>
+			<h1>Header! {currentUser.email}</h1>
+			<Component {...pageProps} />
+		</div>
+	);
+};
+
+AppsComponent.getInitialProps = async (appContext) => {
+	// two kinds of different context ctx is suppose to go to indivudal pages 
+	// and appComponent context is inteded to go to the app component
+	const client = buildClient(appContext.ctx);
+	const { data } = await client.get('/api/users/currentuser');
+	let pageProps = {};
+	if(appContext.Component.getInitialProps) {
+		pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+	}
+
+	return {
+		pageProps, 
+		...data
+	}
+};
+
+export default AppsComponent;
+
--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,12 +1,13 @@
 import buildClient from "../api/build-client";
 
 const LandindPage = ({ currentUser }) => {
-	return currentUser ? <h1>You are signed in</h1> : <h1>you are not signed in</h1>
+	return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 // this is the only location where we can fetch data during server intialization
 // we make a request from the server
 LandindPage.getInitialProps = async (context) => {
+	console.log("Landing Page")
 	const client = buildClient(context);
 	const { data } = await client.get('/api/users/currentuser');
 	return data;
